8b507a85e432404172a17a67c2e773229b944a06
Roncat <flavioroncat@gmail.com>
2017-01-02 19:25:07 -0200

    Update Application.java

--- spring-boot-docker/src/main/java/hello/Application.java
+++ spring-boot-docker/src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello Docker - SpringBoot";
+        return "Hello Docker - Spring Boot";
     }
 
     public static void main(String[] args) {
