4dd5352ab84979618e95bd447c6abb3f3991ac3e
Roncat <flavioroncat@gmail.com>
2017-03-14 00:10:38 -0300

    Add dropwirzard project.

---
+++ dwgettingstarted/src/main/java/com/javaee/dwstart/dwgettingstartedConfiguration.java
@@ -0,0 +1,7 @@
+package com.javaee.dwstart;
+
+import io.dropwizard.Configuration;
+
+public class dwgettingstartedConfiguration extends Configuration {
+    // TODO: implement service configuration
+}
