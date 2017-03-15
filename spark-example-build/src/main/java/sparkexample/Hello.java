a199d5682061b6c3becd36d72efdbbee55b0515d
Flavio <flavioroncat@gmail.com>
2017-03-15 00:41:01 -0300

    update

--- spark-example-build/src/main/java/sparkexample/Hello.java
+++ spark-example-build/src/main/java/sparkexample/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 
     public static void main(String[] args) {
         get("/", (req, res) -> {
-            return "hello - Java Spark project with Docker";
+            return "Hello - Java Spark Framework";
         });
     }
 
