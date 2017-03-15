df9e70eac4c7f2bfb9ab07278dcdbe33c9fe6313
Flavio <flavioroncat@gmail.com>
2017-03-14 23:11:26 -0300

    delete dwgettingstarted

--- dwgettingstarted/src/main/java/com/javaee/dwstart/dwgettingstartedConfiguration.java
+++
@@ -1,7 +0,0 @@
-package com.javaee.dwstart;
-
-import io.dropwizard.Configuration;
-
-public class dwgettingstartedConfiguration extends Configuration {
-    // TODO: implement service configuration
-}
